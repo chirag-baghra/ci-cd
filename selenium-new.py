95d8fc3e76ac2d71d37813ddd3a3159169de80c1
chirag-baghra <54393714+chirag-baghra@users.noreply.github.com>
2024-04-04 22:43:57 -0400

    Update selenium-new.py

--- selenium-new.py
+++ selenium-new.py
@@ -1,6 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
@@ -20,6 +22,10 @@ driver.get('http://127.0.0.1:8000/')
 # # Find an input field and enter text
 
 # input_field.send_keys('test')
+
+# Add a wait of 10 seconds for the element to be located
+wait = WebDriverWait(driver, 10)
+button = wait.until(EC.presence_of_element_located((By.ID, 'submit')))
 
 # # Click a button
 # button = driver.find_element_by_id('button_id')
